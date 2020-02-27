c5df80a2a18c880b1a2161f7ddd4af841b59a16d
enocpi <61128286+enocpi@users.noreply.github.com>
2020-02-27 09:23:18 -0600

    Add files via upload

---
+++ 4_5_LABDANABRIDGES/4_5_LABDANABRIDGES/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+
+namespace _4_5_LABDANABRIDGES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myScores = new ArrayList();
+            myScores.Add(4400);
+            myScores.Add(8900);
+            myScores.Add(1200);
+            myScores.Add(3700);
+            myScores.Add(7200);
+
+            foreach (int score in myScores)
+            {
+                Console.WriteLine(score);
+            }
+
+            myScores.Sort();
+
+            foreach (int score in myScores)
+            {
+                Console.WriteLine(score);
+            }
+
+            myScores.Reverse();
+
+            foreach (int score in myScores)
+            {
+
+                Console.WriteLine(score);
+            }
+
+            myScores.Add(2400);
+
+            foreach (int score in myScores)
+            {
+                Console.WriteLine(score);
+            }
+
+
+            myScores.Remove(4400);
+
+            foreach(int score in myScores)
+            {
+                Console.WriteLine(score);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
